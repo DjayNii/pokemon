259ba0fc0bb27186c38b01802315b2d9f9bc32e5
DjayNii <nithinreddyk24@gmail.com>
2024-04-09 15:42:13 +0530

    added stats and desgin changes

--- index.js
+++ index.js
@@ -7,27 +7,41 @@ async function fetchData() {
       `https://pokeapi.co/api/v2/pokemon-species/${name}`
     );
 
+    const targetDiv = document.querySelector(".card");
     if (!response.ok && !promise.ok) {
       throw new Error("Could not get Pokemon data");
     }
 
     const data = await response.json();
     const data2 = await promise.json();
-    pokemonSprtie = data.sprites.other.home.front_default;
-
-    const types = data.types;
-    const gen = data2.generation.name;
     console.log(data);
     console.log(data2);
 
-    let html = `<img src="${pokemonSprtie}" alt="pokemonsprite" class="pokemonimg" />
+    pokemonSprtie = data.sprites.other.home.front_default;
+    const gen = data2.generation.name;
+    const hp = data.stats[0].base_stat;
+    const statAttack = data.stats[1].base_stat;
+    const statDefense = data.stats[2].base_stat;
+    const statSpeed = data.stats[5].base_stat;
+
+    targetDiv.innerHTML = `<p><span>HP: </span>${hp}</p>
+    <img src="${pokemonSprtie}" alt="pokemonsprite" class="pokemonimg" />
     <h1 class="pokeName">  ${name}</h1>
-    <p class = "gen" >${gen}</p>`;
-
-    const targetDiv = document.querySelector(".card");
-    targetDiv.innerHTML = html;
-
-    show();
+    <p class = "gen" >${gen}</p>
+    <div class="stats">
+    <div>
+      <h3>${statAttack}</h3>
+      <p>Attack</p>
+    </div>
+    <div>
+      <h3>${statDefense}</h3>
+      <p>Defense</p>
+    </div>
+    <div>
+      <h3>${statSpeed}</h3>
+      <p>Speed</p>
+    </div>
+  </div>`;
   } catch (error) {
     console.error(error);
     showError();
@@ -38,8 +52,4 @@ function showError() {
   const errorStatement = document.querySelector(".error");
   errorStatement.style.display = "block";
   errorStatement.style.color = "red";
-}
-
-function show() {
-  const image = document.querySelector(".details");
 }
